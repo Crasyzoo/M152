e8af991bb09c1eeaa3c564ee2d42e8b1e6975c57
Crasyzoo <64598389+Crasyzoo@users.noreply.github.com>
2021-02-01 08:18:35 +0100

    Add files via upload

---
+++ M152/home.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Facebook Page Tab App</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+
+    <div class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                <span class="navbar-toggler-icon"></span>
+            </a>
+            <a class="nav-link" href="#">M152</a>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item"><a class="nav-link" href="home.php">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="post.php">POST</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <div class="float-start">
+        <div class="container-fluid">
+            <p>
+                vdhgfdscgdfsdsghfcd
+            </p>
+        </div>
+    </div>
+
+    <div class="mt-4">
+        <div class="float-end">
+            <div class="position-relative">
+                <div class="container-fluid" class="position-absolute top-50 start-50 translate-middle">
+                    <div class="card" style="width: 30rem;">
+                        <img src="img/Landscape-Color.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div id="fb-root">
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ M152/post.php
@@ -0,0 +1,67 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Facebook Page Tab App</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+
+    <div class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                <span class="navbar-toggler-icon"></span>
+            </a>
+            <a class="nav-link" href="#">M152</a>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item"><a class="nav-link" href="home.php"><i class="bi bi-house-door-fill"></i>Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="post.php">POST</a></li>
+                </ul>
+            </div>
+            <!--/.nav-collapse -->
+        </div>
+    </div>
+
+    <div class="mt-4">
+        <div class="mx-auto">
+            <div class="container-fluid">
+                <div class="text-center">
+                    <form>
+                        <div class="col-auto">
+                        <label class="col-form-label">Image a poster :</label>
+                        </div>
+                        <div class="col-auto">
+                        <input class="form-control" type="file" name="img" id="imgs" accept="image/*" multiple><br>
+                        </div>
+                        <div class="col-auto">
+                        <label class="form-label">Description :</label>
+                        </div>
+                        <div class="col-auto">
+                        <textarea class="col-form-control" name="description"></textarea>
+                        </div>
+
+                        <button class="btn btn-primary" type="submit" name="action">valider</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <div id="fb-root">
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+</body>
+
+</html>
